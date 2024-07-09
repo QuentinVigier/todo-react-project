3305460bb2e650c0199ef06581fd6e67e5c53796
bestdevelope <leabouillot7@gmail.com>
2024-07-09 13:13:33 +0200

    logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,37 +1,42 @@
-import { AiOutlinePlusCircle } from "react-icons/ai"
-import "../styles/header.css"
-import { useState } from "react"
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import "../styles/header.css";
+import { useState } from "react";
+import logo from "../assets/todoLogo.png";
 
 interface HeaderProps {
-    onAddTask: (taskTitle: string) => void;
+  onAddTask: (taskTitle: string) => void;
 }
 
 export function Header({ onAddTask }: HeaderProps) {
+  const [title, setTitle] = useState("");
 
-    const [title, setTitle] = useState("")
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
+    onAddTask(title);
+    setTitle("");
+  }
 
-        onAddTask(title)
-        setTitle("")
-    }
+  function onChangeTitle(e: React.ChangeEvent<HTMLInputElement>) {
+    setTitle(e.target.value);
+  }
 
-    function onChangeTitle(e: React.ChangeEvent<HTMLInputElement>) {
-        setTitle(e.target.value)
-    }
+  return (
+    <header className="header">
+      <img src={logo} alt="todo logo" className="logo" />
 
-    return (
-        <header className='header'>
-            <img src="../assets/todoLogo.svg" alt="todo logo" />
-
-            <form className="newTaskForm" onSubmit={handleSubmit}>
-                <input type="text" placeholder="Add a task" value={title} onChange={onChangeTitle} />
-                <button>
-                    Add
-                    <AiOutlinePlusCircle size={30} />
-                </button>
-            </form>
-        </header>
-    )
+      <form className="newTaskForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a task"
+          value={title}
+          onChange={onChangeTitle}
+        />
+        <button>
+          Add
+          <AiOutlinePlusCircle size={30} />
+        </button>
+      </form>
+    </header>
+  );
 }
